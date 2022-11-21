107ee436ebf8feb3dc15dc2b2195e837a13b3b1b
Huss94 <tendlown@gmail.com>
2022-11-21 14:46:50 +0100

    2 eme commit

--- beamformer_etu.py
+++ beamformer_etu.py
@@ -1,27 +1,29 @@
 import numpy as np #line:1
 N =8 #line:4
 d =0.06 #line:5
-def beam_filter (OOO0000000O00O0O0 ,OO00000OO0O0OOO00 ,O00O0OO00O0000OOO ,OOOO00OOO0O000OO0 =0 ,OO00O0OO0O0O00O0O :int =0 ):#line:7
+def beam_filter (freq_vector ,N ,d ,theta =0 ,mic_nb :int =0 ):#line:7
     ""#line:19
-    OO0OOOO00OOO0O0OO =(OO00O0OO0O0O00O0O -OO00000OO0O0OOO00 -1 )/2 *O00O0OO00O0000OOO #line:22
-    return np .exp (-1j *2 *np .pi *OOO0000000O00O0O0 /340 *OO0OOOO00OOO0O0OO *np .cos (OOOO00OOO0O000OO0 *np .pi /180 ))#line:24
-def beamformer (OOO0OO0000O00O000 ,O0O000OOOO0000OOO ,OO0O000OOOOOO0OO0 ,O0O0OOO0OO0O00OOO ):#line:27
+    z =(mic_nb -N -1 )/2 *d #line:22
+    return np .exp (-1j *2 *np .pi *freq_vector /340 *z *np .cos (theta *np .pi /180 ))#line:24
+
+
+def beamformer (buffer ,theta ,F0 ,Fs ):#line:27
     ""#line:35
-    O000O00OOO00OO000 ,OOO00O0OOO0O0OOO0 =np .shape (OOO0OO0000O00O000 )#line:38
-    OOO00O00O0O0O00O0 =np .arange (0 ,O0O0OOO0OO0O00OOO ,O0O0OOO0OO0O00OOO /OOO00O0OOO0O0OOO0 )#line:41
-    O00OO00O00OOOOO0O =np .zeros ((O000O00OOO00OO000 ,1 ),dtype =np .complex_ )#line:49
-    OO000O0O0O0O0000O =np .zeros ((len (O0O000OOOO0000OOO ),1 ),dtype =np .complex_ )#line:50
-    OO0000OO000OO0OOO =np .fft .fft (OOO0OO0000O00O000 )#line:53
-    OO00OO0OOOOOO00O0 =np .abs (OOO00O00O0O0O00O0 -OO0O000OOOOOO0OO0 ).argmin ()#line:57
-    O0000OO0O0O0O0OOO =OOO00O00O0O0O00O0 [OO00OO0OOOOOO00O0 ]#line:60
-    OO0O0OO0O00OOOO00 =OO0000OO000OO0OOO [:,OO00OO0OOOOOO00O0 ]#line:61
-    for O00OOO0O0000000OO ,OOO00OOOOOO0OO000 in enumerate (O0O000OOOO0000OOO ):#line:64
-        for OO0O0O00OOO00OOOO in np .arange (0 ,O000O00OOO00OO000 ):#line:66
-            OO00OOO00OOO000O0 =beam_filter (O0000OO0O0O0O0OOO ,O000O00OOO00OO000 ,d ,theta =OOO00OOOOOO0OO000 ,mic_id =OO0O0O00OOO00OOOO )#line:68
-            O00OO00O00OOOOO0O [OO0O0O00OOO00OOOO ,:]=OO0O0OO0O00OOOO00 [OO0O0O00OOO00OOOO ]*OO00OOO00OOO000O0 #line:70
-        OO000O0O0O0O0000O [O00OOO0O0000000OO ,:]=sum (O00OO00O00OOOOO0O ,1 )#line:72
-    O0O0O0OOO0O0OO000 =np .sum (np .square (np .abs (OO000O0O0O0O0000O )),1 )#line:75
-    return O0O0O0OOO0O0OO000 #line:77
+    h ,w =np .shape (buffer )#line:38
+    v1 =np .arange (0 ,Fs ,Fs /w )#line:41
+    v2 =np .zeros ((h ,1 ),dtype =np .complex )#line:49
+    v3 =np .zeros ((len (theta ),1 ),dtype =np .complex_ )#line:50
+    v4 =np .fft .fft (buffer )#line:53
+    v5 =np .abs (v1 -F0 ).argmin ()#line:57
+    v6 =v1 [v5 ]#line:60
+    v7 =v4 [:,v5 ]#line:61
+    for v8 ,v9 in enumerate (theta ):#line:64
+        for v10 in np .arange (0 ,h ):#line:66
+            v11 =beam_filter (v6 ,h ,d ,theta =v9 ,mic_id =v10 )#line:68
+            v2 [v10 ,:]=v7 [v10 ]*v11 #line:70
+        v3 [v8 ,:]=sum (v2 ,1 )#line:72
+    v13 =np .sum (np .square (np .abs (v3 )),1 )#line:75
+    return v13 #line:77
 if __name__ =="__main__":#line:79
     print ("Simulation")#line:80
     beamformer (1 )
