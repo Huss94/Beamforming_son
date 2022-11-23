cc70773b50704324e659de9a5196266f43c16240
Huss94 <tendlown@gmail.com>
2022-11-23 01:01:49 +0100

    Manqnue que la question 14

--- beamformer_etu.py
+++ beamformer_etu.py
@@ -1,28 +1,28 @@
 import numpy as np #line:1
 N =8 #line:4
 d =0.06 #line:5
-def beam_filter (freq_vector ,N ,d ,theta =0 ,mic_nb :int =0 ):#line:7
+def beam_filter (freq_vector ,N ,d ,theta =0 ,mic_id :int =0 ):#line:7
     ""#line:19
-    z =(mic_nb -N -1 )/2 *d #line:22
+    z =(mic_id -N -1 )/2 *d #line:22
     return np .exp (-1j *2 *np .pi *freq_vector /340 *z *np .cos (theta *np .pi /180 ))#line:24
 
 
 def beamformer (buffer ,theta ,F0 ,Fs ):#line:27
     ""#line:35
-    h ,w =np .shape (buffer )#line:38
-    v1 =np .arange (0 ,Fs ,Fs /w )#line:41
-    v2 =np .zeros ((h ,1 ),dtype =np .complex )#line:49
-    v3 =np .zeros ((len (theta ),1 ),dtype =np .complex_ )#line:50
-    v4 =np .fft .fft (buffer )#line:53
-    v5 =np .abs (v1 -F0 ).argmin ()#line:57
-    v6 =v1 [v5 ]#line:60
-    v7 =v4 [:,v5 ]#line:61
-    for v8 ,v9 in enumerate (theta ):#line:64
-        for v10 in np .arange (0 ,h ):#line:66
-            v11 =beam_filter (v6 ,h ,d ,theta =v9 ,mic_id =v10 )#line:68
-            v2 [v10 ,:]=v7 [v10 ]*v11 #line:70
-        v3 [v8 ,:]=sum (v2 ,1 )#line:72
-    v13 =np .sum (np .square (np .abs (v3 )),1 )#line:75
+    N ,BLK =np .shape (buffer )#line:38
+    Freq =np .arange (0 ,Fs ,Fs /BLK )#line:41
+    v2 =np .zeros ((N ,1 ),dtype =np .complex )#line:49
+    Y_th =np .zeros ((len (theta ),1 ),dtype =np .complex_ )#line:50
+    Mfft =np .fft .fft (buffer )#line:53
+    k0 =np .abs (Freq -F0 ).argmin ()#line:57
+    Fk0 =Freq [k0 ]#line:60
+    M =Mfft [:,k0 ]#line:61
+    for i ,th in enumerate (theta ):#line:64
+        for n in np .arange (0 ,N ):#line:66
+            W =beam_filter (Fk0 ,N ,d ,theta =th ,mic_id =n )#line:68
+            v2 [n ,:]=M [n ]*W #line:70
+        Y_th [i ,:]=sum (v2 ,1 )#line:72
+    v13 =np .sum (np .square (np .abs (Y_th )),1 )#line:75
     return v13 #line:77
 if __name__ =="__main__":#line:79
     print ("Simulation")#line:80
