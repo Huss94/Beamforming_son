7f1139cb2c0899084cac5bd829a61bf77583be0f
Huss94 <tendlown@gmail.com>
2022-11-30 16:03:14 +0100

    correction des fautes fautes d'orthographe

---
+++ test/beamformer_etu.py
@@ -0,0 +1,33 @@
+import numpy as np #line:1
+N =8 #line:4
+d =0.06 #line:5
+# j'ai modifié le nom des variables pour vor si j'avais bien compris
+
+def beam_filter (freq_vector ,N ,d ,theta =0 ,mic_id :int =0 ):#line:7
+    ""#line:19
+    z =(mic_id -N -1 )/2 *d #line:22
+    return np .exp (-1j *2 *np .pi *freq_vector /340 *z *np .cos (theta *np .pi /180 ))#line:24
+
+
+def beamformer (buffer ,theta ,F0 ,Fs ):#line:27
+    ""#line:35
+    N ,BLK =np .shape (buffer )#line:38
+    Freq =np .arange (0 ,Fs ,Fs /BLK )#line:41
+    v2 =np .zeros ((N ,1 ),dtype =np .complex )#line:49
+    Y_th =np .zeros ((len (theta ),1 ),dtype =np .complex_ )#line:50
+    Mfft =np .fft .fft (buffer )#line:53
+    
+    # J'ai repris cette idée pour trouver k0 qui était bien plus propre et fiable que ma façon de faire 
+    k0 =np .abs (Freq -F0 ).argmin ()#line:57
+    Fk0 =Freq [k0 ]#line:60
+    M =Mfft [:,k0 ]#line:61
+    for i ,th in enumerate (theta ):#line:64
+        for n in np .arange (0 ,N ):#line:66
+            W =beam_filter (Fk0 ,N ,d ,theta =th ,mic_id =n )#line:68
+            v2 [n ,:]=M [n ]*W #line:70
+        Y_th [i ,:]=sum (v2 ,1 )#line:72
+    v13 =np .sum (np .square (np .abs (Y_th )),1 )#line:75
+    return v13 #line:77
+if __name__ =="__main__":#line:79
+    print ("Simulation")#line:80
+    beamformer (1 )
